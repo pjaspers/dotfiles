55aa73dd1639c0c3c52ae4534263bcdcefbbb2dd
pjaspers <piet@jaspe.rs>
2014-12-15 11:06:29 +0100

    Don't try to load go if it's not installed.

--- zsh/golang.zsh
+++ zsh/golang.zsh
@@ -1,5 +1,8 @@
-# For godoc and go vet
-export GOROOT=`go env GOROOT`
-export PATH=$PATH:$GOROOT/bin
-# Development in go path
-export GOPATH=~/development/go
+if (( $+commands[go] )) ;
+then
+    # For godoc and go vet
+    export GOROOT=`go env GOROOT`
+    export PATH=$PATH:$GOROOT/bin
+    # Development in go path
+    export GOPATH=~/development/go
+fi
