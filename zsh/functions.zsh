a9a76731081a2c797b0f013e41f4ff45061f9612
pjaspers <piet@10to1.be>
2016-01-04 22:01:18 +0100

    A better wifi monitor.
    Apple's wifi bars are a lie. I've opened up a radar, they're fine with
    it being a lie. [0]
    
    If you're signal is > -85 you're pretty much guaranteed to have five
    bars, even if the noise is as high as the signal, which kills your
    wifi and renders it useless.
    
    They're probably doing this because you can't measure the noise without
    joining the network. So a network that appeared 5 bars could drop to 0
    bars when joined, this would confuse users. Solving it would be too Snow
    Leopard-esque.
    
    [0] http://openradar.appspot.com/radar?id=5032012842795008

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -260,3 +260,30 @@ function ftco() {
 
     curl -sSI $1 | grep "location:" | awk '{print $2}' | tee >(pbcopy)
 }
+
+# Monitor the wifi
+#
+# tldr; Apple's wifi bars are a lie. http://openradar.appspot.com/radar?id=5032012842795008
+#
+# To better find out what's happening this prints out the signal strength (📶), the noise (📢)
+# the delta between signal and noise (Δ) and the throughput (📈)
+#
+# How to read this:
+#
+# If signal is > -70: your wifi is fine
+# If signal is > -85 and < -70, look at the Δ, the higher the better.
+# If signal is < -85, your wifi is the opposite of fine.
+#
+# Called without arguments it will print it once, called with an argument it will keep polling.
+function ai() {
+    airport_bin="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
+    ruby -e 'puts "%3s %3s %3s %3s" %  ["📶", "📢", "Δ", "📈"]'
+    if [[ $1 ]] && times=99999 || times=1
+    while true; do
+          $airport_bin -I |
+              grep -E "CtlRSSI|CtlNoise|lastTxRate" |
+              ruby -ne 'name, value = $_.split(": "); r ||= Hash.new(0); r[name.strip.gsub("agrCtl", "")] = value.strip; r["SNR"] = (r["RSSI"].to_f - r["Noise"].to_f); values = [r["RSSI"], r["Noise"], r["SNR"], r["lastTxRate"]]; print "\r%3d %3d %3d %3d" % values if r.keys.length == 4'
+          ((times--))
+          if (( $times > 0)); then sleep .5; else break; fi
+    done
+}
