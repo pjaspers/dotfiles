525a0f5f394f5e06a9a68d3c5d690666ec0ea13b
pjaspers <piet@pjaspers.com>
2017-06-28 00:09:09 +0200

    Move airport checker to own bin

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -244,36 +244,6 @@ function ftco() {
     curl -sSI $1 | grep "location:" | awk '{print $2}' | tee >(pbcopy)
 }
 
-# Monitor the wifi
-#
-# tldr; Apple's wifi bars are a lie. http://openradar.appspot.com/radar?id=5032012842795008
-#
-# To better find out what's happening this prints out the signal strength (📶), the noise (📢)
-# the delta between signal and noise (Δ) and the throughput (📈)
-#
-# How to read this:
-#
-# -10__________________-73________-100 (dBm)
-# Max                 Strong      Minimum
-#
-# -10 > signal > -73 => your wifi is fine
-# -73 > signal > -85 => look at the Δ, the higher the better.
-# -85 > signal       => your wifi is the opposite of fine.
-#
-# Called without arguments it will print it once, called with an argument it will keep polling.
-function ai() {
-    airport_bin="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
-    ruby -e 'puts "%3s %3s %3s %3s" %  ["📶", "📢", "Δ", "📈"]'
-    if [[ $1 ]] && times=99999 || times=1
-    while true; do
-          $airport_bin -I |
-              grep -E "CtlRSSI|CtlNoise|lastTxRate" |
-	            ruby -ne 'def c(rssi, &block); rssi = rssi.to_f;bad, ok, meh = [31,32,33];mod = ok;mod = meh if rssi < -73;mod = bad if rssi < -85;result = block.call.to_s;print "\e[1;#{mod}m#{result}\e[0m";end; name, value = $_.split(": "); r ||= Hash.new(0); r[name.strip.gsub("agrCtl", "")] = value.strip; r["SNR"] = (r["RSSI"].to_f - r["Noise"].to_f); values = [r["RSSI"], r["Noise"], r["SNR"], r["lastTxRate"]]; c(r["RSSI"]) { "\r%3d %3d %3d %3d" % values } if r.keys.length == 4'
-          ((times--))
-          if (( $times > 0)); then sleep .5; else break; fi
-    done
-}
-
 # Takes current branch and tries to deploy it using capistrano
 function dp() {
     if [[ $1 ]] && env=$1 || env="staging"
