c8797bf1f0681f6f792657bcd2766bc4fe53844f
pjaspers <piet@pjaspers.com>
2022-11-19 21:05:44 +0100

    Also support changing activities

--- lisp/piek.lisp
+++ lisp/piek.lisp
@@ -6,6 +6,7 @@
   (:export :toplevel :*ui*))
 
 (in-package :piek)
+
 (defvar *api-token* (uiop:getenv "HASS_TOKEN"))
 (defvar *hass-url* (uiop:getenv "HASS_IP"))
 (defparameter *debug-is-on* nil)
@@ -21,6 +22,18 @@
    (available :initarg :available))
   (:report (lambda (c s)
              (format s "Unsupported command: ~A, should be one of ~A" (slot-value c 'used-command) (slot-value c 'available)))))
+
+(define-condition unknown-device(user-error)
+  ((used-device :initarg :used-device)
+   (available :initarg :available))
+  (:report (lambda (c s)
+             (format s "Unsupported device: ~A, should be one of ~A" (slot-value c 'used-device) (slot-value c 'available)))))
+
+(define-condition unknown-activity(user-error)
+  ((used-activity :initarg :used-activity)
+   (available :initarg :available))
+  (:report (lambda (c s)
+             (format s "Unsupported device: ~A, should be one of ~A" (slot-value c 'used-activity) (slot-value c 'available)))))
 
 (defun ensure-response-string (response)
   (etypecase response
@@ -42,69 +55,64 @@
 (defun list-activities(conf)
   (jso-keys (st-json:getjso "Activities" conf)))
 
-;; (assoc "-1" (slot-value (st-json:getjso "Activities" *harmony-conf*) 'st-json::alist) :test #'string=)
-;; (mapcar #'cdr (slot-value (st-json:getjso "Activities" *harmony-conf*) 'st-json::alist))
-;; (mapcar #'car (slot-value (st-json:getjso "Activities" *harmony-conf*) 'st-json::alist))
-
 (defstruct device name id commands)
-(defstruct activity name id)
 (defun json->device(name jso)
   (let ((commands (st-json:getjso "commands" jso))
          (id (st-json:getjso "id" jso)))
     (make-device :name name :id id :commands commands)
     ))
 
+(defstruct activity name id)
+
+(defun activities-from-conf(conf)
+  (let* ((thing nil)
+        (mapper (lambda(k v) (push (make-activity :name v :id k) thing))))
+
+        (st-json:mapjso mapper (st-json:getjso "Activities" conf))
+    thing))
+
 (defun devices-from-conf(conf)
   (let ((thing nil))
     (st-json:mapjso #'(lambda (k v)
                         (push (json->device k v) thing)
-                        ) (st-json:getjso "Devices" *harmony-conf*))
+                        ) (st-json:getjso "Devices" conf))
     thing))
+
+(defun activities()
+  (activities-from-conf *harmony-conf*))
 
 (defun devices()
   (devices-from-conf *harmony-conf*)
   )
 
-(defun device-with-id (id)
-  (find id (devices-from-conf *harmony-conf*) :key 'device-id :test 'equal))
 
-(defun device-with-name (name)
-  (find name (devices-from-conf *harmony-conf*) :key 'device-name :test 'equal))
-
-(defun device-matches-name (name)
-  (let ((scanner (cl-ppcre:create-scanner name :case-insensitive-mode t)))
-    (find name (devices) :test #'(lambda (a b) (cl-ppcre:scan scanner (device-name b))))
+(defmacro with-key(kind key value)
+  (let* ((plural (cond((equal kind "device") "devices") ((equal kind "activity") "activities")))
+         (lookup (intern (string-upcase (concatenate 'string (format nil "~A" plural) "-from-conf"))))
+        (accessor (intern (string-upcase (concatenate 'string kind "-" key)))))
+        ;; `(find value (,lookup ,*harmony-conf*) :key (intern (concatenate 'string type "-" key)) :test 'equal))
+        `(find ,value (,lookup *harmony-conf*) :key (quote ,accessor) :test 'equal)
     ))
 
-(defun device-name-from-id(identifier jso)
-  (cl-ppcre:scan "lg" (string-downcase "LG"))
-  ;; (mapcar #'(lambda(k) (st:json:getjso* (format nil "Devices.~a.id" k) *harmony-conf*)) names)
+(defun collection(kind)
+  (let* ((plural (cond((equal kind "device") "devices") ((equal kind "activity") "activities"))))
+    (funcall (intern (string-upcase plural) "PIEK"))
+    )
+  )
+
+(defun with-name-matches(kind name)
+  (let ((scanner (cl-ppcre:create-scanner name :case-insensitive-mode t))
+        (objects (collection kind))
+        (accessor (intern (string-upcase (concatenate 'string kind "-name")) "PIEK")))
+    (find name objects :test #'(lambda (a b) (declare (ignore a)) (cl-ppcre:scan scanner (funcall accessor b))))
+    ))
+
+(defun device-with-id (id) (with-key "device" "id" id))
+(defun device-with-name (name) (with-key "device" "name" name))
 
 
-  (let ((names (jso-keys (st-json:getjso "Devices" *harmony-conf*))))
-    (mapcar #'(lambda(k)
-                ;; (st-json:getjso* (format nil "Devices.~a.id" k) *harmony-conf*)
-                ;; (cons k (st-json:getjso "id" (st-json:getjso k (st-json:getjso "Devices" *harmony-conf*))))
-                (let ((path (format nil "Devices.~s.id" k)))
-                  (format t "Path: |~a|" path)
-                  (type-of "Devices.Apple TV.id")
-                  (type-of (string-downcase path))
-                  (type-of path)
-                  (st-json:getjso* (string-downcase path) *harmony-conf*)
-                  (type-of "Devices.Apple TV.id")
-                  (st-json:getjso* `(,@(path)) *harmony-conf*)
-                  )
-                ) names))
-
-
-
-
-  (st-json:getjso* "Devices.LG TV (2).id" *harmony-conf*)
-  (st-json:getjso* "Devices.Telenet DVR.id" *harmony-conf*)
-  (st-json:getjso* "Devices.Apple TV.id" *harmony-conf*)
-  (st-json:getjso* "Devices.LG TV.id" *harmony-conf*)
-  (st-json:mapjso #'(lambda (k v) (format t "K: ~a V: ~a" k v)) *harmony-conf*)
-  (type-of (st-json:getjso "Devices" *harmony-conf*))
+(defun device-matches-name (name)
+  (with-name-matches "device" name)
   )
 
 (defun debug-info-send-command(hass-url entity-id device-id commands)
@@ -112,10 +120,11 @@
 /api/service/remote/send_command
 
   [hass-url] ~A
+  [entity]   ~A
   [device]   ~A
   [commands] ~A
 
-" *hass-url* entity-id device-id commands))
+" hass-url entity-id device-id commands))
 
 ;; http://100.74.48.103:8123/api/services/remote/send_command
 (defun send-command(&key (entity-id "remote.piek") (device) (commands ()))
@@ -126,7 +135,7 @@
 
 
   (position "poweron" (device-commands device) :test #'(lambda(a b) (equal (string-downcase a) (string-downcase b))))
-  (multiple-value-bind (body status headers uri stream needs-close reason)
+  (multiple-value-bind (body status uri reason)
       (drakma:http-request (concatenate 'string "http://" *hass-url* ":8123/api/services/remote/send_command")
                            :content (st-json:write-json-to-string
                                      (st-json:jso
@@ -142,11 +151,35 @@
         (error "Received ~D ~A from ~A: ~A" status reason uri body))
     ))
 
+(defun debug-info-send-activity(hass-url entity-id activity-id)
+  (format t "
+/api/service/remote/turn_on
 
-;; (st-json:write-json-to-string (st-json:jso "bob" "stinkt" "so" "beit"))
-;; (st-json:write-json-to-string (apply #'st-json:jso (alexandria:flatten '(("bob" "stinkt") ("so" "beit")))))
+  [hass-url] ~A
+  [entity]   ~A
+  [activity] ~A
 
+" hass-url entity-id activity-id))
 
+(defun send-activity(&key (entity-id "remote.piek") (activity))
+  (if *debug-is-on*
+      (debug-info-send-activity *hass-url* entity-id (activity-id activity)))
+
+  (multiple-value-bind (body status uri reason)
+      (drakma:http-request (concatenate 'string "http://" *hass-url* ":8123/api/services/remote/turn_on")
+                           :content (st-json:write-json-to-string
+                                     (st-json:jso
+                                      "entity_id" entity-id
+                                      "activity" (activity-id activity)))
+                           :content-type "application/json"
+                           :method :post
+                           :additional-headers (list (cons "Authorization" (format nil "Bearer ~a" *api-token*))))
+    (setf body (ensure-response-string body))
+    (if (<= 200 status 299)
+        (st-json:read-json body)
+        (error "Received ~D ~A from ~A: ~A" status reason uri body))
+    )
+  )
 
 (defparameter *help*
   (adopt:make-option 'help
@@ -170,13 +203,21 @@
     :initial-value "lg"
     :reduce #'adopt:last))
 
+(defparameter *activity*
+  (adopt:make-option 'activity
+    :help (format nil "send activity to harmony")
+    :long "activity"
+    :short #\a
+    :parameter "ACTIVITY"
+    :reduce #'adopt:last))
+
 (defparameter *ui*
   (adopt:make-interface
     :name "piek"
     :usage "[-d DEVICE COMMAND]"
     :summary "control tv through harmony through hass"
     :help "Control the TV, or Apple TV, or Telenet"
-    :contents (list *help* *device* *option-debug* *option-no-debug*)
+    :contents (list *help* *device* *option-debug* *option-no-debug* *activity*)
     :examples '(("Volume Down TV':" . "piek -d lg volumedown")
                 ("Pause Apple TV:" . "piek -d apple pause")
                 ("Pause Telenet:" . "piek -d telenet pause")
@@ -185,9 +226,26 @@
 (defun print-available-commands(device)
   (format t "~A" (device-commands device)))
 
+(defun print-available-activities()
+  (format t "~A" (mapcar #'activity-name (activities))))
+
+(defun handle-activity(arguments activity-name)
+  (let ((activity (with-name-matches "activity" activity-name)))
+    (cond
+      ((equal "list" (car arguments)) (print-available-activities))
+      ((null activity) (error 'unknown-activity :available (mapcar #'activity-name (activities)) :used-activity activity-name))
+      (t (send-activity :activity activity)))))
+
+(defun handle-device(arguments device-name)
+  (let ((device (device-matches-name device-name)))
+    (cond
+      ((equal "list" (car arguments)) (print-available-commands device))
+      ((null device) (error 'unknown-device :available (mapcar #'device-name (devices)) :used-device device-name))
+      (t (send-command :device device :commands arguments)))))
+
 ;; todo: also support direct id for the device
 (defun toplevel ()
-  (sb-ext:disable-debugger)
+  ;; (sb-ext:disable-debugger)
   (handler-case
       (multiple-value-bind (arguments options) (adopt:parse-options *ui*)
         (when (gethash 'debug options)
@@ -196,11 +254,12 @@
           (setf *debug-is-on* t))
         (when (gethash 'help options)
           (adopt:print-help-and-exit *ui*))
+        (when (gethash 'activity options)
+          (handle-activity arguments (gethash 'activity options))
+          (adopt:exit))
+        (when (gethash 'device options)
+          (handle-device arguments (gethash 'device options))
+          (adopt:exit))
         (if (null arguments)
-            (adopt:print-help-and-exit *ui*))
-        (let ((device (device-matches-name (gethash 'device options))))
-          (cond
-            ((equal "commands" (car arguments)) (print-available-commands device))
-            ((null device) (format t "Unknown device, should be one of: ~{ ~a ~}" (list-device-names *harmony-conf*)))
-            (t (send-command :device device :commands arguments)))))
+            (adopt:print-help-and-exit *ui*)))
     (error (c) (adopt:print-error-and-exit c))))
